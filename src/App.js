249a32eea5d3790ef643c33dc19fd03bf30b687f
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-08-19 23:42:43 +0600

    Service details almost done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from './components/Banner/Banner';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/Services/Services';
+import ServiceDetails from './components/ServiceDetails/ServiceDetails';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
 	return (
@@ -13,6 +15,10 @@ const App = () => {
 				</Route>
 				<Route path='/home'>
 					<Banner />
+				</Route>
+				<Route path='/details'>
+					<Navbar />
+					<ServiceDetails />
 				</Route>
 			</Switch>
 		</Router>
---
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const ServiceDetails = () => {
+	return (
+		<div className='text-center mt-4'>
+			<h4 className='uppercase text-md text-red-500'>Face Your Products</h4>
+			<h1 className='my-8 text-6xl text-primary font-light'>
+				Photograph Grocery and <br /> Packaged Products
+			</h1>
+			<div className='w-8/12 mx-auto text-2xl font-light text-gray-600 leading-relaxed'>
+				<p>
+					loOrtery manufactures a variety of in house photography solutions that
+					take professional product shots of grocery items on pure white or
+					transparent backgrounds. In addition, our all-in-one photography
+					solutions and software can be used to create interactive 360 photos,
+					3D animations and videos. Perfect systems for beginners and
+					professional photographers alike. create interactive 360 photos, 3D
+					animations and videos. Perfect systems for beginners and professional
+					photographers alike.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default ServiceDetails;
