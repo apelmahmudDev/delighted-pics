30fdde62a8499532ea08bb88f434b600025ddf56
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-08-19 21:00:26 +0600

    Added Search box

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,23 +1,12 @@
-import { SearchIcon } from '@heroicons/react/outline';
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import SearchBox from '../SearchBox/SearchBox';
 
 const Banner = () => {
 	return (
 		<div className='bg-delight-banner relative bg-cover bg-center bg-no-repeat h-screen w-screen'>
 			<Navbar />
-			<div>
-				<h1 className='text-white text-4xl font-bold'>
-					What Type of Photography service <br /> Are You Looking For?
-				</h1>
-				<div className='flex items-center'>
-					<span>
-						<SearchIcon className='h-6 w-6' aria-hidden='true' />
-					</span>
-					<input type='text' placeholder='Type your Photography service...' />
-					<button>Search</button>
-				</div>
-			</div>
+			<SearchBox />
 		</div>
 	);
 };
---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { SearchIcon } from '@heroicons/react/outline';
+
+const SearchBox = () => {
+	return (
+		<div className='flex items-center justify-center h-96 max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+			<div>
+				<h1 className='text-white text-4xl font-bold leading-relaxed mb-8 text-center'>
+					What Type of Photography service <br /> Are You Looking For?
+				</h1>
+				<div className='flex items-center bg-white h-12 w-full rounded-lg overflow-hidden'>
+					<SearchIcon className='w-12 text-gray-400 px-2' aria-hidden='true' />
+					<input
+						className='w-full focus:outline-none text-lg'
+						type='text'
+						placeholder='Type your Photography service...'
+					/>
+					<button className='bg-red-500 hover:bg-red-600 text-white h-full px-8 text-lg font-bold'>
+						Search
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SearchBox;
