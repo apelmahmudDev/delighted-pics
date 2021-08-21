77d98ac9c39cb26d41211bedd70617533875481f
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-08-21 06:38:33 +0600

    Order form design complete

--- src/components/OrderInfo/OrderInfo.js
+++ src/components/OrderInfo/OrderInfo.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useAuth } from '../../contexts/AuthContext';
 
 const OrderInfo = () => {
+	const { currentUser } = useAuth();
 	const {
 		register,
 		handleSubmit,
@@ -11,38 +13,70 @@ const OrderInfo = () => {
 	const onSubmit = (data) => console.log(data);
 
 	return (
-		<div className='shadow'>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<div className='my-8'>
-					<label className='text-primary font-semibold'>Name</label>
+		<div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 my-12'>
+			<div className='shadow border p-4 rounded-lg'>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className='my-8'>
+						<label className='text-primary font-semibold'>Name</label>
+						<input
+							defaultValue={currentUser?.displayName || 'David doll'}
+							{...register('name', { required: true })}
+							placeholder='Enter your name'
+							className='form-control'
+						/>
+						{errors.name && (
+							<span className='text-red-500 italic text-sm'>
+								Name field is required
+							</span>
+						)}
+					</div>
+					<div className='my-8'>
+						<label className='text-primary font-semibold'>Email</label>
+						<input
+							defaultValue={currentUser?.email || 'example@gmail.com'}
+							{...register('email', { required: true })}
+							placeholder='Enter your Email'
+							className='form-control'
+						/>
+						{errors.email && (
+							<span className='text-red-500 italic text-sm'>
+								Email field is required
+							</span>
+						)}
+					</div>
+					<div className='my-8'>
+						<label className='text-primary font-semibold'>Phone</label>
+						<input
+							{...register('phone', { required: true })}
+							placeholder='Phone number'
+							className='form-control'
+						/>
+						{errors.phone && (
+							<span className='text-red-500 italic text-sm'>
+								Phone field is required
+							</span>
+						)}
+					</div>
+					<div className='my-8'>
+						<label className='text-primary font-semibold'>Address</label>
+						<input
+							{...register('address', { required: true })}
+							placeholder='Address'
+							className='form-control'
+						/>
+						{errors.address && (
+							<span className='text-red-500 italic text-sm'>
+								Address field is required
+							</span>
+						)}
+					</div>
 					<input
-						defaultValue='David Doll'
-						{...register('nameRequired', { required: true })}
-						placeholder='Enter your name'
-						className='form-control'
+						type='submit'
+						value='Submit'
+						className='p-2  bg-purple-400 hover:bg-purple-600 text-white uppercase rounded'
 					/>
-					{errors.nameRequired && (
-						<span className='text-red-500 italic text-sm'>
-							Name field is required
-						</span>
-					)}
-				</div>
-				<div className='my-8'>
-					<label className='text-primary font-semibold'>Name</label>
-					<input
-						defaultValue='test'
-						{...register('exampleRequired', { required: true })}
-						placeholder='Enter your name'
-						className='form-control'
-					/>
-					{errors.exampleRequired && (
-						<span className='text-red-500 italic text-sm'>
-							This field is required
-						</span>
-					)}
-				</div>
-				<input type='submit' value='Submit' className='p-2  bg-purple-400' />
-			</form>
+				</form>
+			</div>
 		</div>
 	);
 };
