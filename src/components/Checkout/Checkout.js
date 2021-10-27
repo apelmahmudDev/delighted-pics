a55236d5cb6d3505111c444e9854903f08ab3e95
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-27 23:29:12 +0600

    border changed

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
-import CheckoutTableData from '../CheckoutTableData/CheckoutTableData';
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import CheckoutTableData from "../CheckoutTableData/CheckoutTableData";
 
 const Checkout = () => {
 	const { currentUser, setIsNavigate } = useAuth();
 	const [orders, setOrders] = useState([]);
 	const [loading, setLoading] = useState(false);
-	const [deleteMessage, setDeleteMessage] = useState('');
+	const [deleteMessage, setDeleteMessage] = useState("");
 
 	useEffect(() => {
 		setIsNavigate(true);
@@ -23,7 +23,7 @@ const Checkout = () => {
 				const data = await response.json();
 				setOrders(data);
 			} catch (error) {
-				console.log('err', error);
+				console.log("err", error);
 			}
 			setLoading(false);
 		};
@@ -33,68 +33,68 @@ const Checkout = () => {
 	// delete service from the cart
 	const handleDeleteService = async (serviceId) => {
 		try {
-			setDeleteMessage('');
+			setDeleteMessage("");
 			const url = `https://fierce-river-40368.herokuapp.com/delete/${serviceId}`;
 			const option = {
-				method: 'DELETE',
+				method: "DELETE",
 				headers: {
-					'Content-type': 'application/json; charset=UTF-8',
+					"Content-type": "application/json; charset=UTF-8",
 				},
 			};
 			const response = await fetch(url, option);
 			const data = await response.json();
 			if (data) {
-				setDeleteMessage('Your cart service deleted successfully');
+				setDeleteMessage("Your cart service deleted successfully");
 				setTimeout(() => {
-					setDeleteMessage('');
+					setDeleteMessage("");
 					window.location.reload();
 				}, 3000);
 			}
 		} catch (error) {
-			console.log('err', error);
+			console.log("err", error);
 		}
 	};
 
 	const tableHeader = [
-		'Services',
-		'Service fee',
-		'quality',
-		'Status',
-		'Remove',
+		"Services",
+		"Service fee",
+		"quality",
+		"Status",
+		"Remove",
 	];
 	return (
-		<div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 my-12'>
-			{loading && <div className='text-center text-lg'>Loading...</div>}
+		<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 my-12">
+			{loading && <div className="text-center text-lg">Loading...</div>}
 			{!loading && (
-				<div className='flex flex-col'>
-					<div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
-						<div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
-							<div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
+				<div className="flex flex-col">
+					<div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+						<div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+							<div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
 								{/* show delete message */}
-								<div className='text-right p-3 text-green-500 text-lg font-semibold'>
+								<div className="text-right p-3 text-green-500 text-lg font-semibold">
 									<h3>{deleteMessage}</h3>
 								</div>
-								<table className='min-w-full divide-y divide-gray-200'>
-									<thead className='bg-gray-50'>
+								<table className="min-w-full divide-y divide-gray-200">
+									<thead className="bg-gray-50">
 										<tr>
 											{tableHeader.map((header) => (
 												<th
 													key={header}
-													scope='col'
-													className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+													scope="col"
+													className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
 												>
 													{header}
 												</th>
 											))}
 										</tr>
 									</thead>
-									<tbody className='bg-white divide-y divide-gray-200'>
+									<tbody className="bg-white divide-y divide-gray-200">
 										{/* show message when cart is empty */}
 										{!orders.length && (
 											<tr>
 												<td
-													colSpan='5'
-													className='text-center text-xl text-red-500 font-semibold p-8'
+													colSpan="5"
+													className="text-center text-xl text-red-500 font-semibold p-8"
 												>
 													You have no service
 												</td>
--- src/components/OrderInfo/OrderInfo.js
+++ src/components/OrderInfo/OrderInfo.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useHistory, useParams } from 'react-router-dom';
-import { useAuth } from '../../contexts/AuthContext';
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useHistory, useParams } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 const OrderInfo = () => {
 	const [service, setService] = useState({});
 	const [loading, setLoading] = useState(false);
-	const [message, setMessage] = useState('');
+	const [message, setMessage] = useState("");
 
 	const { buyId } = useParams();
 	const { currentUser, setIsNavigate } = useAuth();
@@ -29,29 +29,29 @@ const OrderInfo = () => {
 			serviceName: service.name,
 			fee: service.fee,
 			quality: service.quality,
-			status: 'Progressing',
+			status: "Progressing",
 			date: new Date(),
 		};
 		// add orders info at mongodb
 		try {
-			setMessage('');
+			setMessage("");
 			setLoading(true);
-			const url = 'https://fierce-river-40368.herokuapp.com/addOrder';
+			const url = "https://fierce-river-40368.herokuapp.com/addOrder";
 			const option = {
-				method: 'POST',
+				method: "POST",
 				body: JSON.stringify(orderInfo),
 				headers: {
-					'Content-type': 'application/json; charset=UTF-8',
+					"Content-type": "application/json; charset=UTF-8",
 				},
 			};
 			const response = await fetch(url, option);
 			const data = await response.json();
 			if (data) {
-				setMessage('Your order successfully placed to us!');
-				history.push('/checkout');
+				setMessage("Your order successfully placed to us!");
+				history.push("/checkout");
 			}
 		} catch (error) {
-			console.log('err', error);
+			console.log("err", error);
 		}
 	};
 
@@ -66,7 +66,7 @@ const OrderInfo = () => {
 				const data = await response.json();
 				setService(data);
 			} catch (error) {
-				console.log('err', error);
+				console.log("err", error);
 			}
 			setLoading(false);
 		};
@@ -74,80 +74,80 @@ const OrderInfo = () => {
 	}, [buyId]);
 
 	return (
-		<div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 my-12'>
-			<div className='my-4'>
-				<h2 className='text-2xl text-primary font-lighter'>
+		<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 my-12">
+			<div className="my-4">
+				<h2 className="text-2xl text-primary font-lighter">
 					Your service {service.name}
 				</h2>
-				<p className='text-lg text-primary'>
+				<p className="text-lg text-primary">
 					Service fee $<span>{service.fee}</span>
 				</p>
 			</div>
 
-			<div className='shadow border p-4 rounded-lg'>
+			<div className="border-t-4 border-gray-400 shadow p-4 rounded-lg">
 				<form onSubmit={handleSubmit(onSubmit)}>
-					<div className='my-8'>
-						<label className='text-primary font-semibold'>Name</label>
+					<div className="my-8">
+						<label className="text-primary font-semibold">Name</label>
 						<input
-							defaultValue={currentUser?.displayName || ''}
-							{...register('name', { required: true })}
-							placeholder='Enter your name'
-							className='form-control border'
+							defaultValue={currentUser?.displayName || ""}
+							{...register("name", { required: true })}
+							placeholder="Enter your name"
+							className="form-control border"
 						/>
 						{errors.name && (
-							<span className='text-red-500 italic text-sm'>
+							<span className="text-red-500 italic text-sm">
 								Name field is required
 							</span>
 						)}
 					</div>
-					<div className='my-8'>
-						<label className='text-primary font-semibold'>Email</label>
+					<div className="my-8">
+						<label className="text-primary font-semibold">Email</label>
 						<input
-							defaultValue={currentUser?.email || ''}
-							{...register('email', { required: true })}
-							placeholder='Enter your Email'
-							className='form-control border'
+							defaultValue={currentUser?.email || ""}
+							{...register("email", { required: true })}
+							placeholder="Enter your Email"
+							className="form-control border"
 						/>
 						{errors.email && (
-							<span className='text-red-500 italic text-sm'>
+							<span className="text-red-500 italic text-sm">
 								Email field is required
 							</span>
 						)}
 					</div>
-					<div className='my-8'>
-						<label className='text-primary font-semibold'>Phone</label>
+					<div className="my-8">
+						<label className="text-primary font-semibold">Phone</label>
 						<input
-							{...register('phone', { required: true })}
-							placeholder='Phone number'
-							className='form-control border'
+							{...register("phone", { required: true })}
+							placeholder="Phone number"
+							className="form-control border"
 						/>
 						{errors.phone && (
-							<span className='text-red-500 italic text-sm'>
+							<span className="text-red-500 italic text-sm">
 								Phone field is required
 							</span>
 						)}
 					</div>
-					<div className='my-8'>
-						<label className='text-primary font-semibold'>Address</label>
+					<div className="my-8">
+						<label className="text-primary font-semibold">Address</label>
 						<input
-							{...register('address', { required: true })}
-							placeholder='Address'
-							className='form-control border'
+							{...register("address", { required: true })}
+							placeholder="Address"
+							className="form-control border"
 						/>
 						{errors.address && (
-							<span className='text-red-500 italic text-sm'>
+							<span className="text-red-500 italic text-sm">
 								Address field is required
 							</span>
 						)}
 					</div>
-					<div className='flex items-center'>
+					<div className="flex items-center">
 						<input
-							type='submit'
+							type="submit"
 							disabled={loading}
-							value='Order now'
-							className='p-2  bg-primary hover:bg-secondary text-white uppercase rounded'
+							value="Order now"
+							className="p-2  bg-primary hover:bg-secondary text-white uppercase rounded"
 						/>
-						<p className='text-md text-green-500 ml-4'>{message}</p>
+						<p className="text-md text-green-500 ml-4">{message}</p>
 					</div>
 				</form>
 			</div>
