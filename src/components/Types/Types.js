069012d0a304fdcd0547e86f6a0a7d68b99fcf94
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-30 23:59:46 +0600

    refactor and create lib dir

--- src/components/Types/Types.js
+++ src/components/Types/Types.js
@@ -1,57 +1,7 @@
 import React from "react";
-import Portrait from "../../images/types/front-camera.png";
-import Photojournalism from "../../images/types/photo.png";
-import dress from "../../images/types/dress.png";
-import sports from "../../images/types/sports.png";
-import workLife from "../../images/types/work-life.png";
-import architecture from "../../images/types/architecture.png";
+import photographyTypes from "../../lib/photographyTypes";
 
 const Types = () => {
-	const types = [
-		{
-			id: 1,
-			title: "Portrait Photography",
-			image: Portrait,
-			details:
-				"One of the most common photography styles, portrait photography, or portraiture",
-		},
-		{
-			id: 2,
-			title: "Photojournalism",
-			image: Photojournalism,
-			details:
-				"One of the most common photography styles, portrait photography, or portraiture",
-		},
-		{
-			id: 3,
-			title: "Fashion Photography",
-			image: dress,
-			details:
-				"One of the most common photography styles, portrait photography, or portraiture",
-		},
-		{
-			id: 4,
-			title: "Sports Photography",
-			image: sports,
-			details:
-				"One of the most common photography styles, portrait photography, or portraiture",
-		},
-		{
-			id: 5,
-			title: "Still Life Photography",
-			image: workLife,
-			details:
-				"One of the most common photography styles, portrait photography, or portraiture",
-		},
-		{
-			id: 6,
-			title: "Architectural Photography",
-			image: architecture,
-			details:
-				"One of the most common photography styles, portrait photography, or portraiture",
-		},
-	];
-
 	return (
 		<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 my-20">
 			<div className="text-center my-16">
@@ -59,14 +9,14 @@ const Types = () => {
 					Photography Types
 				</h1>
 				<p className="text-primary text-md">
-					We have included our photography styles that's can help you to find
-					your choose images by <br /> recording light, either electronically by
-					means of an image!
+					We have included our photography styles types that's can help you to
+					find any services <br /> that you want. We delight with you for this
+					awesome
 				</p>
 			</div>
 			{/* photography types */}
 			<div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-4 md:gap-8">
-				{types.map((type) => (
+				{photographyTypes.map((type) => (
 					<div
 						key={type.id}
 						className="bg-gray-50 rounded-md p-3 shadow sm:h-72 flex justify-center items-center"
@@ -80,7 +30,7 @@ const Types = () => {
 							<h3 className="text-xl text-primary font-medium my-3">
 								{type.title}
 							</h3>
-							<p>{type.details}</p>
+							<p className="text-primary text-md">{type.details}</p>
 						</div>
 					</div>
 				))}
---
+++ src/lib/photographyTypes.js
@@ -0,0 +1,53 @@
+import Portrait from "../images/types/front-camera.png";
+import Photojournalism from "../images/types/photo.png";
+import dress from "../images/types/dress.png";
+import sports from "../images/types/sports.png";
+import workLife from "../images/types/work-life.png";
+import architecture from "../images/types/architecture.png";
+
+const photographyTypes = [
+	{
+		id: 1,
+		title: "Portrait Photography",
+		image: Portrait,
+		details:
+			"One of the most common photography styles, portrait photography, or portraiture",
+	},
+	{
+		id: 2,
+		title: "Photojournalism",
+		image: Photojournalism,
+		details:
+			"Photojournalism is a way of telling the story of a newsworthy event photographs.",
+	},
+	{
+		id: 3,
+		title: "Fashion Photography",
+		image: dress,
+		details:
+			"Fashion photography showcases fashion clothing, shoes, and accessories",
+	},
+	{
+		id: 4,
+		title: "Sports Photography",
+		image: sports,
+		details:
+			"sports photographs can depict the passion, emotion that fuels sporting events.",
+	},
+	{
+		id: 5,
+		title: "Still Life Photography",
+		image: workLife,
+		details:
+			"Like it sounds, still life photography features inanimate objects—natural or manmade",
+	},
+	{
+		id: 6,
+		title: "Architectural Photography",
+		image: architecture,
+		details:
+			"interior, exterior design of buildings are the subject of architectural photography",
+	},
+];
+
+export default photographyTypes;
