3601513493f43f6482fb4ef1e4267d96e3cc2b96
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-11-02 19:18:05 +0600

    example static done

--- src/components/Example/Example.js
+++ src/components/Example/Example.js
@@ -2,13 +2,19 @@ import React, { useEffect } from "react";
 import "./Example.css";
 import { useAuth } from "../../contexts/AuthContext";
 import photoExample1 from "../../images/example/example2.jpg";
-import photoExample2 from "../../images/example/example2.jpg";
+import photoExample2 from "../../images/example/example9.jpg";
 import photoExample3 from "../../images/example/example3.jpg";
 import photoExample4 from "../../images/example/example4.jpg";
 import photoExample5 from "../../images/example/example5.jpg";
 import photoExample6 from "../../images/example/example6.jpg";
 import photoExample7 from "../../images/example/example7.jpg";
 import photoExample8 from "../../images/example/example8.jpg";
+import photoExample10 from "../../images/example/example10.jpg";
+import photoExample11 from "../../images/example/example11.jpg";
+import photoExample12 from "../../images/example/example12.jpg";
+import photoExample13 from "../../images/example/example13.jpg";
+import photoExample14 from "../../images/example/example14.jpg";
+import photoExample15 from "../../images/example/example15.jpg";
 import ExampleNavigation from "./ExampleNavigation/ExampleNavigation";
 
 const Example = () => {
@@ -26,7 +32,12 @@ const Example = () => {
 		{ id: 5, image: photoExample5, spreadArea: "row-span-2" },
 		{ id: 6, image: photoExample6, spreadArea: "row-span-2 col-span-2" },
 		{ id: 7, image: photoExample7, spreadArea: "col-span-2" },
+		{ id: 11, image: photoExample12 },
 		{ id: 8, image: photoExample8, spreadArea: "row-span-2" },
+		{ id: 9, image: photoExample10, spreadArea: "row-span-2 col-span-2" },
+		{ id: 13, image: photoExample13, spreadArea: "row-span-2" },
+		{ id: 14, image: photoExample14, spreadArea: "col-span-2" },
+		{ id: 15, image: photoExample15, spreadArea: "row-span-2 col-span-2" },
 	];
 
 	return (
@@ -34,15 +45,16 @@ const Example = () => {
 			<ExampleNavigation />
 
 			<div className="container grid grid-flow-row-dense gap-4">
-				{photoExamples.map((image) => (
-					<div key={image.id} className={`${image.spreadArea}`}>
-						<img
-							className="w-full h-full object-cover"
-							src={image.image}
-							alt="photoExample1"
-						/>
-					</div>
-				))}
+				{photoExamples &&
+					photoExamples.map((image) => (
+						<div key={image.id} className={`${image?.spreadArea}`}>
+							<img
+								className="w-full h-full object-cover"
+								src={image?.image}
+								alt="photoExample1"
+							/>
+						</div>
+					))}
 			</div>
 		</div>
 	);
