7337986f522f9b9f00f71f1f3757991823b41014
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-06-12 19:51:03 +0600

    👽api key changed

--- src/components/Dashboard/AddService/AddService.js
+++ src/components/Dashboard/AddService/AddService.js
@@ -1,12 +1,12 @@
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import axios from 'axios';
-import React, { useRef, useState } from 'react';
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import React, { useRef, useState } from "react";
 
 const AddService = () => {
 	const [isUploading, setIsUpLoading] = useState(false);
-	const [message, setMessage] = useState('');
-	const [imageUrl, setImageUrl] = useState('');
+	const [message, setMessage] = useState("");
+	const [imageUrl, setImageUrl] = useState("");
 
 	// catch input value by using useRef hook
 	const nameRef = useRef();
@@ -19,11 +19,11 @@ const AddService = () => {
 	const imageUploadHandler = (e) => {
 		setIsUpLoading(true);
 		const imageData = new FormData();
-		imageData.set('key', 'ca5482cb4e564b594544191602467167'); // set api key
-		imageData.append('image', photoRef.current.files[0]);
+		imageData.set("key", process.env.REACT_APP_IMAGE_API_KEY); // set api key
+		imageData.append("image", photoRef.current.files[0]);
 
 		axios
-			.post('https://api.imgbb.com/1/upload', imageData)
+			.post("https://api.imgbb.com/1/upload", imageData)
 			.then((res) => {
 				setImageUrl(res.data.data.url);
 				setIsUpLoading(false);
@@ -47,108 +47,108 @@ const AddService = () => {
 
 		// add service info at mongodb
 		try {
-			setMessage('');
-			const url = 'https://fierce-river-40368.herokuapp.com/addService';
+			setMessage("");
+			const url = "https://fierce-river-40368.herokuapp.com/addService";
 			const option = {
-				method: 'POST',
+				method: "POST",
 				body: JSON.stringify(service),
 				headers: {
-					'Content-type': 'application/json; charset=UTF-8',
+					"Content-type": "application/json; charset=UTF-8",
 				},
 			};
 			const response = await fetch(url, option);
 			const data = await response.json();
 			if (data) {
-				setMessage('Your service added into database successfully.');
-				nameRef.current.value = '';
-				feeRef.current.value = '';
-				qualityRef.current.value = '';
-				descriptionRef.current.value = '';
+				setMessage("Your service added into database successfully.");
+				nameRef.current.value = "";
+				feeRef.current.value = "";
+				qualityRef.current.value = "";
+				descriptionRef.current.value = "";
 			}
 		} catch (error) {
-			console.log('err', error);
+			console.log("err", error);
 		}
 	};
 
 	return (
-		<div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 my-12'>
-			<div className='bg-white rounded-xl p-4'>
+		<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 my-12">
+			<div className="bg-white rounded-xl p-4">
 				<form onSubmit={handleAddService}>
-					<div className='mt-4 md:flex justify-around gap-2'>
+					<div className="mt-4 md:flex justify-around gap-2">
 						<div>
-							<div className='mb-4'>
-								<label className='font-semibold'>Service Name</label>
+							<div className="mb-4">
+								<label className="font-semibold">Service Name</label>
 								<input
-									type='text'
+									type="text"
 									ref={nameRef}
-									className='form-control'
+									className="form-control"
 									required
-									placeholder='Enter service name'
+									placeholder="Enter service name"
 								/>
 							</div>
-							<div className='mb-4'>
-								<label className='font-semibold'>Add Fee</label>
+							<div className="mb-4">
+								<label className="font-semibold">Add Fee</label>
 								<input
-									type='text'
+									type="text"
 									ref={feeRef}
-									className='form-control'
+									className="form-control"
 									required
-									placeholder='Enter service fee'
+									placeholder="Enter service fee"
 								/>
 							</div>
-							<div className='mb-4'>
-								<label className='font-semibold'>Description</label>
+							<div className="mb-4">
+								<label className="font-semibold">Description</label>
 								<textarea
-									type='text'
+									type="text"
 									ref={descriptionRef}
-									rows='4'
-									className='form-control'
+									rows="4"
+									className="form-control"
 									required
-									placeholder='Enter Service description'
+									placeholder="Enter Service description"
 								/>
 							</div>
 						</div>
 						<div>
-							<div className='mb-4'>
-								<label className='font-semibold'>Quality</label>
+							<div className="mb-4">
+								<label className="font-semibold">Quality</label>
 								<input
-									type='text'
+									type="text"
 									ref={qualityRef}
-									className='form-control'
+									className="form-control"
 									required
-									placeholder='Enter service quality'
+									placeholder="Enter service quality"
 								/>
 							</div>
-							<div className='mb-4'>
-								<label className='font-semibold'>Add Photo</label>
+							<div className="mb-4">
+								<label className="font-semibold">Add Photo</label>
 								<input
-									type='file'
+									type="file"
 									onChange={imageUploadHandler}
 									ref={photoRef}
 									required
-									className='form-control'
+									className="form-control"
 								/>
 							</div>
 							{/* image uploading message by spinner */}
 							{isUploading && (
-								<div className='font-semibold text-primary ml-2'>
-									<span className='mr-3 '>Uploading</span>
+								<div className="font-semibold text-primary ml-2">
+									<span className="mr-3 ">Uploading</span>
 									<FontAwesomeIcon
 										icon={faSpinner}
 										spin
-										size='lg'
-										className='text-primary'
+										size="lg"
+										className="text-primary"
 									/>
 								</div>
 							)}
 						</div>
 					</div>
-					<div className='text-right'>
-						<div className='text-md text-green-500 mb-4'>{message}</div>
+					<div className="text-right">
+						<div className="text-md text-green-500 mb-4">{message}</div>
 						<input
-							type='submit'
-							className='px-4 py-2 rounded text-primary border main-btn'
-							value='Save'
+							type="submit"
+							className="px-4 py-2 rounded text-primary border main-btn"
+							value="Save"
 						/>
 					</div>
 				</form>
